ed772659edde1739a1db034ebbdf7f0b1fc6156b
NerRobDog <119906947+NerRobDog@users.noreply.github.com>
2026-09-05 14:27:00 +0200

    ci: close remaining 21 unresolved (static curl, censor client, roll/stat types, md4/md5, perfcounter iface)

--- cmake/pwci_extra.cmake
+++ cmake/pwci_extra.cmake
@@ -13,12 +13,26 @@ set( ALL_SRCS ${ALL_SRCS}
   ${SRC_DIR}/Server/ServerAppBase/NivalService.cpp
   ${SRC_DIR}/Server/Monitoring/PerfCounterProvider/PerfCounter.cpp
   ${SRC_DIR}/Game/PF/HybridServer/RPeered.auto.cpp
+  ${SRC_DIR}/Game/PF/Server/Statistic/SpecialStatisticTypes.cpp
+  ${SRC_DIR}/Game/PF/Server/Roll/RollTypes.cpp
+  ${SRC_DIR}/Game/PF/Server/Censorship/CensorClient.cpp
+  ${SRC_DIR}/Game/PF/Server/Censorship/CensorJob.cpp
+  ${SRC_DIR}/Game/PF/Server/Censorship/CensorSettings.cpp
+  ${SRC_DIR}/Server/Monitoring/PerfCounterProvider/LPerfCounterProviderIface.auto.cpp
+  ${VENDOR}/MD4/md4c.c
+  ${VENDOR}/MD4/md5c.c
   ${SRC_DIR}/pwci_stubs.cpp
 )
+
+# Vendor/libcurl/lib/Release/libcurl.lib is a static build (exports _curl_*, no __imp__),
+# same as Nival's vcproj which defined CURL_STATICLIB. Static curl pulls winsock + ldap.
+add_definitions( -DCURL_STATICLIB )
 include_directories( ${SRC_DIR}/Server/Monitoring ${SRC_DIR}/Game/PF/Server )
 
 # --- vendor / tool libs ---
 simple_add_library( wbemuuid )
+simple_add_library( ws2_32 )
+simple_add_library( wldap32 )
 simple_add_library( ${VENDOR}/JsonCpp/lib/Release/JsonCpp )
 simple_add_library( ${VENDOR}/libcurl/lib/Release/libcurl )
 simple_add_library( ${SRC_DIR}/../Tools/Censor/lib/Release/CensorDll )
